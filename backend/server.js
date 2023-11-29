8b3a52e749b2b4d5e8346c8f5c499e6d0342f44e
nirjal <nirjalprajapati@gmail.com>
2023-11-29 17:00:52 +0545

    backend first commit

---
+++ backend/server.js
@@ -0,0 +1,17 @@
+import express from "express";
+import products from "./data/products";
+const port = 5000;
+
+const app = express();
+
+app.get("/", (req, res, next) => {
+  res.send("API is running...");
+});
+
+app.get("/api/products", (req, res) => {
+  res.json(products);
+});
+
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
